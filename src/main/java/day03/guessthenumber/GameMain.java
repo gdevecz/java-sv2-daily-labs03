a836f519d79a4279c165301d8485f2ea17b826d2
gdevecz <gdevecz@gmail.com>
2021-10-27 11:53:05 +0200

    A számkitalálós játék megoldása. game.UUPPERBOUND és game.LOVERBOUND javítva GuessTheGame.LOWERBOUND és GuessTheGame.UPPERBOUND-ra. Konvenciós hiba. Osztályváltozóra nem példányon kereesztül hivatkozunk.

--- src/main/java/day03/guessthenumber/GameMain.java
+++ src/main/java/day03/guessthenumber/GameMain.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class GameMain {
 
     public static void main(String[] args) {
+
         // Mivel lehet direkt volt így: 1 és 100 között, a lehetséges elemek: 2 - 99
         GuessTheNumber game = new GuessTheNumber();
         System.out.println("A program kitalál egy véletlenszerű számot " +
-                game.LOWERBOUND + " és " +
-                game.UPPERBOUND + " között.");
+                GuessTheNumber.LOWERBOUND + " és " +
+                GuessTheNumber.UPPERBOUND + " között.");
         System.out.println("Ki kell találnod, 6 lépésben melyik számra gondolt.");
 
         System.out.println("Enter-re indul a játék!");
